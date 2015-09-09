c853f975854ff35a137b52145ac7b20887c80372
BARRAUD Fabien <barraudf@gmail.com>
2015-09-09 20:28:24 +0200

    Project updated to Unity 5.2.0f3 - UnityVS no longer needed - Tests have been disabled until Unity Test Tools is compatible

--- Assets/Editor/Tests/Tools/DirectionsTest.cs
+++ Assets/Editor/Tests/Tools/DirectionsTest.cs
@@ -1,28 +1,28 @@
-﻿using UnityEngine;
-using NUnit.Framework;
+﻿//using UnityEngine;
+//using NUnit.Framework;
 
-[TestFixture]
-public class DirectionsTest
-{
-	[Test]
-	public void OppositeTest()
-	{
-		Assert.AreEqual(Direction.Down, Direction.Up.Opposite(), "#1");
-		Assert.AreEqual(Direction.Up, Direction.Down.Opposite(), "#2");
-		Assert.AreEqual(Direction.Left, Direction.Right.Opposite(), "#3");
-		Assert.AreEqual(Direction.Right, Direction.Left.Opposite(), "#4");
-		Assert.AreEqual(Direction.Backward, Direction.Forward.Opposite(), "#5");
-		Assert.AreEqual(Direction.Forward, Direction.Backward.Opposite(), "#6");
-	}
+//[TestFixture]
+//public class DirectionsTest
+//{
+//	[Test]
+//	public void OppositeTest()
+//	{
+//		Assert.AreEqual(Direction.Down, Direction.Up.Opposite(), "#1");
+//		Assert.AreEqual(Direction.Up, Direction.Down.Opposite(), "#2");
+//		Assert.AreEqual(Direction.Left, Direction.Right.Opposite(), "#3");
+//		Assert.AreEqual(Direction.Right, Direction.Left.Opposite(), "#4");
+//		Assert.AreEqual(Direction.Backward, Direction.Forward.Opposite(), "#5");
+//		Assert.AreEqual(Direction.Forward, Direction.Backward.Opposite(), "#6");
+//	}
 
-	[Test]
-	public void NormalTest()
-	{
-		Assert.AreEqual(Vector3.down, Direction.Down.ToUnitVector(), "#1");
-		Assert.AreEqual(Vector3.up, Direction.Up.ToUnitVector(), "#2");
-		Assert.AreEqual(Vector3.left, Direction.Left.ToUnitVector(), "#3");
-		Assert.AreEqual(Vector3.right, Direction.Right.ToUnitVector(), "#4");
-		Assert.AreEqual(Vector3.back, Direction.Backward.ToUnitVector(), "#5");
-		Assert.AreEqual(Vector3.forward, Direction.Forward.ToUnitVector(), "#6");
-	}
-}
+//	[Test]
+//	public void NormalTest()
+//	{
+//		Assert.AreEqual(Vector3.down, Direction.Down.ToUnitVector(), "#1");
+//		Assert.AreEqual(Vector3.up, Direction.Up.ToUnitVector(), "#2");
+//		Assert.AreEqual(Vector3.left, Direction.Left.ToUnitVector(), "#3");
+//		Assert.AreEqual(Vector3.right, Direction.Right.ToUnitVector(), "#4");
+//		Assert.AreEqual(Vector3.back, Direction.Backward.ToUnitVector(), "#5");
+//		Assert.AreEqual(Vector3.forward, Direction.Forward.ToUnitVector(), "#6");
+//	}
+//}
--- Assets/Editor/Tests/VoxelEngine/World/WorldChunkTest.cs
+++ Assets/Editor/Tests/VoxelEngine/World/WorldChunkTest.cs
@@ -1,53 +1,53 @@
-﻿using UnityEngine;
-using NUnit.Framework;
-using System;
+﻿//using UnityEngine;
+//using NUnit.Framework;
+//using System;
 
-[TestFixture]
-public class WorldChunkTest
-{
-	[Test]
-	public void CalculateChunkOffsetTest()
-	{
-		WorldChunk chunk = new WorldChunk(null, GridPosition.Zero);
-		chunk.InitBlocks(5, 6, 7);
+//[TestFixture]
+//public class WorldChunkTest
+//{
+//	[Test]
+//	public void CalculateChunkOffsetTest()
+//	{
+//		WorldChunk chunk = new WorldChunk(null, GridPosition.Zero);
+//		chunk.InitBlocks(5, 6, 7);
 
-		GridPosition p1 = chunk.CalculateChunkOffset(-11, 18, 5);
-		GridPosition p2 = chunk.CalculateChunkOffset(5, 6, 7);
-		GridPosition p3 = chunk.CalculateChunkOffset(24, -12, -6);
+//		GridPosition p1 = chunk.CalculateChunkOffset(-11, 18, 5);
+//		GridPosition p2 = chunk.CalculateChunkOffset(5, 6, 7);
+//		GridPosition p3 = chunk.CalculateChunkOffset(24, -12, -6);
 
-		Assert.AreEqual(-3, p1.x, "#1");
-		Assert.AreEqual(3, p1.y, "#2");
-		Assert.AreEqual(0, p1.z, "#3");
+//		Assert.AreEqual(-3, p1.x, "#1");
+//		Assert.AreEqual(3, p1.y, "#2");
+//		Assert.AreEqual(0, p1.z, "#3");
 
-		Assert.AreEqual(1, p2.x, "#4");
-		Assert.AreEqual(1, p2.y, "#5");
-		Assert.AreEqual(1, p2.z, "#6");
+//		Assert.AreEqual(1, p2.x, "#4");
+//		Assert.AreEqual(1, p2.y, "#5");
+//		Assert.AreEqual(1, p2.z, "#6");
 
-		Assert.AreEqual(4, p3.x, "#7");
-		Assert.AreEqual(-2, p3.y, "#8");
-		Assert.AreEqual(-1, p3.z, "#9");
-	}
+//		Assert.AreEqual(4, p3.x, "#7");
+//		Assert.AreEqual(-2, p3.y, "#8");
+//		Assert.AreEqual(-1, p3.z, "#9");
+//	}
 
-	[Test]
-	public void CalculateBlockPositionTest()
-	{
-		WorldChunk chunk = new WorldChunk(null, GridPosition.Zero);
-		chunk.InitBlocks(5, 6, 7);
+//	[Test]
+//	public void CalculateBlockPositionTest()
+//	{
+//		WorldChunk chunk = new WorldChunk(null, GridPosition.Zero);
+//		chunk.InitBlocks(5, 6, 7);
 
-		GridPosition p1 = chunk.CalculateBlockPosition(-11, 18, 5);
-		GridPosition p2 = chunk.CalculateBlockPosition(5, 6, 7);
-		GridPosition p3 = chunk.CalculateBlockPosition(24, -12, -6);
+//		GridPosition p1 = chunk.CalculateBlockPosition(-11, 18, 5);
+//		GridPosition p2 = chunk.CalculateBlockPosition(5, 6, 7);
+//		GridPosition p3 = chunk.CalculateBlockPosition(24, -12, -6);
 
-		Assert.AreEqual(4, p1.x, "#1");
-		Assert.AreEqual(0, p1.y, "#2");
-		Assert.AreEqual(5, p1.z, "#3");
+//		Assert.AreEqual(4, p1.x, "#1");
+//		Assert.AreEqual(0, p1.y, "#2");
+//		Assert.AreEqual(5, p1.z, "#3");
 
-		Assert.AreEqual(0, p2.x, "#4");
-		Assert.AreEqual(0, p2.y, "#5");
-		Assert.AreEqual(0, p2.z, "#6");
+//		Assert.AreEqual(0, p2.x, "#4");
+//		Assert.AreEqual(0, p2.y, "#5");
+//		Assert.AreEqual(0, p2.z, "#6");
 
-		Assert.AreEqual(4, p3.x, "#7");
-		Assert.AreEqual(0, p3.y, "#8");
-		Assert.AreEqual(1, p3.z, "#9");
-	}
-}
+//		Assert.AreEqual(4, p3.x, "#7");
+//		Assert.AreEqual(0, p3.y, "#8");
+//		Assert.AreEqual(1, p3.z, "#9");
+//	}
+//}
